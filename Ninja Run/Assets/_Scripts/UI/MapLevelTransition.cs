5e96af9b4fb7b2d406d73672acfe5af9d7ae2d68
realRepeace <raphi.paris@outlook.com>
2023-01-25 09:59:00 +0100

    Cloud Transition and new Map system added

---
+++ Ninja Run/Assets/_Scripts/UI/MapLevelTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class MapLevelTransition : MonoBehaviour
+{
+
+    public float movePositionX = 70f;
+    public float resetPositionX = 700f;
+    public GameObject worldSelection;
+    public GameObject levelSelection;
+    // Start is called before the first frame update
+
+
+    public void CloudTransition()
+    {
+        LeanTween.moveLocal(gameObject, new Vector3(movePositionX, 0f, 0f), 0.4f).setDelay(0.2f);
+        StartCoroutine(ChangeScene(1.2f));
+    }
+
+    private IEnumerator ChangeScene(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        worldSelection.SetActive(false);
+        levelSelection.SetActive(true);
+        LeanTween.moveLocal(gameObject, new Vector3(resetPositionX, 0f, 0f), 0.4f);
+    }
+}
