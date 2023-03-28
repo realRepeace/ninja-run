9a394cfe05a6d45158063ae06eec1fda8bccc01e
realRepeace <raphi.paris@outlook.com>
2023-03-28 09:31:07 +0200

    Skin system added

--- Ninja Run/Assets/_Scripts/Managers/SkinManager.cs
+++ Ninja Run/Assets/_Scripts/Managers/SkinManager.cs
@@ -1,36 +1,35 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.U2D.Animation;
 
 public class SkinManager : MonoBehaviour
 {
-    public Sprite[] skins;
-    public Image skinPreviewUI;
-    
+    public SpriteLibraryAsset[] skins;
+
     int currentSkin = 0;
 
-    public static Sprite selectedSkin;
-    // Start is called before the first frame update
     void Start()
     {
         currentSkin = PlayerPrefs.GetInt("currentSkin", 0);     //beim ersten Spielstart ist der Standard-Skin ausgewählt
-        skinPreviewUI.sprite = skins[currentSkin];  
-        Debug.Log(currentSkin);
+        ChangeSkin(skins[currentSkin]);
     }
 
     public void NextSkin()
     {
         currentSkin = (currentSkin + 1) % skins.Length;
-        skinPreviewUI.sprite = skins[currentSkin];
         PlayerPrefs.SetInt("currentSkin", currentSkin);
-        selectedSkin = skins[currentSkin];
+        ChangeSkin(skins[currentSkin]);
     }
 
     public void PreviousSkin()
     {
         currentSkin = (currentSkin - 1) % skins.Length;
         if (currentSkin < 0) currentSkin = skins.Length - 1;
-        skinPreviewUI.sprite = skins[currentSkin];
         PlayerPrefs.SetInt("currentSkin", currentSkin);
-        selectedSkin = skins[currentSkin];
+        ChangeSkin(skins[currentSkin]);
+    }
+
+    public void ChangeSkin(SpriteLibraryAsset skin)
+    {
+        GetComponent<SpriteLibrary>().spriteLibraryAsset = skin;
     }
 }
