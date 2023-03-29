afaf83de0e8776e6980bc9e7d1e03d0eb8f7fe11
realRepeace <raphi.paris@outlook.com>
2023-03-29 22:54:33 +0200

    Player Idle in UI + Explosion added

--- Ninja Run/Assets/_Scripts/Wurfstern.cs
+++ Ninja Run/Assets/_Scripts/Wurfstern.cs
@@ -11,6 +11,7 @@ public class Wurfstern : MonoBehaviour          //regelt das Verhalten der Wurfs
 
     private float rangeLimit = 11f;
     private float horizontalKnockback = 3f;
+    private float explosionRadius = 3f;
 
 
     void Update()
@@ -30,7 +31,7 @@ public class Wurfstern : MonoBehaviour          //regelt das Verhalten der Wurfs
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
+        if (other.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent) && !canExplode)
         {
             enemyComponent.TakeDamage(damage);
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalKnockback, ForceMode2D.Impulse);
@@ -41,8 +42,23 @@ public class Wurfstern : MonoBehaviour          //regelt das Verhalten der Wurfs
         }
         if (canExplode)
         {
-            Instantiate(explosionParticle, other.transform.position, transform.rotation);
+            Explode(other);
         }
         Destroy(gameObject);
     } 
+
+    private void Explode(Collider2D other)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+	    Instantiate(explosionParticle, other.transform.position, transform.rotation);
+
+        foreach(Collider2D hit in colliders)
+        {
+            if (hit.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
+            {
+                enemyComponent.TakeDamage(damage);
+                hit.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * horizontalKnockback, ForceMode2D.Impulse);
+            }
+        }
+    }
 }
